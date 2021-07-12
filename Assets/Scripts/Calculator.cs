119ca387c8f6aface6c15c9bb23f8d5d204e38e3
Sergio Garzon <sggarzonutn@gmail.com>
2021-07-12 02:14:33 -0300

    Nuevo proyecto

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Calculator : MonoBehaviour
+{
+    public GameObject panel1;
+    public GameObject panel2;
+
+
+    public void PressButton()
+    {
+        panel1.gameObject.SetActive(false);
+        panel2.gameObject.SetActive(true);
+    }
+    
+
+
+
+
+}
